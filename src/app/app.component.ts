7d8e18294de837fbd979fb27ae6497521cee58ad
bounis <bounis@olbati.com>
2019-03-04 00:18:07 +0100

    products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Article} from './article/article.model';
+import {Product} from "./Product";
 
 @Component({
   selector: 'app-root',
@@ -7,25 +8,56 @@ import {Article} from './article/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  articles: Article[];
+
+  products: Product[];
 
   constructor() {
-    this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1),
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
     ];
   }
 
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(title.value + ' ' + link.value);
-    this.articles.push(new Article(title.value, link.value, 0));
-    title.value = '';
-    link.value = '';
-    return false;
+  productWasSelected(product: Product): void {
+    console.log("product was picked", product);
   }
 
-  sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => a.votes - b.votes);
-  }
+
+  // articles: Article[];
+  //
+  // constructor() {
+  //   this.articles = [
+  //     new Article('Angular 2', 'http://angular.io', 3),
+  //     new Article('Fullstack', 'http://fullstack.io', 2),
+  //     new Article('Angular Homepage', 'http://angular.io', 1),
+  //   ];
+  // }
+  //
+  // addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+  //   console.log(title.value + ' ' + link.value);
+  //   this.articles.push(new Article(title.value, link.value, 0));
+  //   title.value = '';
+  //   link.value = '';
+  //   return false;
+  // }
+  //
+  // sortedArticles(): Article[] {
+  //   return this.articles.sort((a: Article, b: Article) => a.votes - b.votes);
+  // }
 }
