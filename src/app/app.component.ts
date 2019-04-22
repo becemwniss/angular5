ec12f087f2b75aa60d99a9b810862d4b51ed7331
bounis <bounis@olbati.com>
2019-04-23 00:18:18 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // keyWord: string;
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+  }
 
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder, private http: HttpClient){
+    this.myForm = fb.group({
+      'word': ['dd']
+    });
+  }
   makeRequest(keyWord: string):void{
-    console.log("this is a test ", keyWord);
+    this.http.get('http://localhost:8080/test').subscribe((data: string) => {
+      console.log(data);
+    });
   }
 
 
